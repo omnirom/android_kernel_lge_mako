7c10449877d4f0f0359077f4e48528ff879a47f7
Dmitry Shmidt <dimitrysh@google.com>
2015-04-08 13:28:11 -0700

    Power: Report total suspend times from boot in suspend_since_boot
        This node exports five values separated by space.
            From left to right:
            1. Amount of suspend/resume cycles
            2. Amount of suspend abort cycles
            3. Total time spent in suspend/resume process
            4. Total time in suspend abort process
            5. Total time spent sleep in suspend state
    
    Change-Id: Ife188fd8386dce35f95fa7ba09fbc9d7e152db62
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -48,8 +48,14 @@ static unsigned long wakeup_ready_nowait;
 
 static struct timespec last_xtime; /* wall time before last suspend */
 static struct timespec curr_xtime; /* wall time after last suspend */
-static struct timespec last_stime; /* total_sleep_time before last suspend */
-static struct timespec curr_stime; /* total_sleep_time after last suspend */
+static struct timespec last_stime; /* sleep time before last suspend */
+static struct timespec curr_stime; /* sleep time after last suspend */
+
+static struct timespec total_xtime; /* total suspend time since boot */
+static struct timespec total_stime; /* total sleep time since boot */
+static struct timespec total_atime; /* total suspend abort time since boot */
+static unsigned long suspend_count; /* total amount of resumes */
+static unsigned long abort_count;   /* total amount of suspend abort */
 
 static void init_wakeup_irq_node(struct wakeup_irq_node *p, int irq)
 {
@@ -296,10 +302,12 @@ static ssize_t suspend_since_boot_show(struct kobject *kobj,
 
 static struct kobj_attribute resume_reason = __ATTR_RO(last_resume_reason);
 static struct kobj_attribute suspend_time = __ATTR_RO(last_suspend_time);
+static struct kobj_attribute suspend_since_boot = __ATTR_RO(suspend_since_boot);
 
 static struct attribute *attrs[] = {
 	&resume_reason.attr,
 	&suspend_time.attr,
+	&suspend_since_boot.attr,
 	NULL,
 };
 static struct attribute_group attr_group = {
@@ -545,11 +553,8 @@ static bool delete_node(struct wakeup_irq_node *n, void *unused)
 	return true;
 }
 
-void clear_wakeup_reasons(void)
+static void clear_wakeup_reasons_nolock(void)
 {
-	unsigned long flags;
-	spin_lock_irqsave(&resume_reason_lock, flags);
-
 	BUG_ON(logging_wakeup_reasons());
 	walk_irq_node_tree(base_irq_nodes, delete_node, NULL);
 	base_irq_nodes = NULL;
@@ -557,7 +562,14 @@ void clear_wakeup_reasons(void)
 	cur_irq_tree_depth = 0;
 	INIT_LIST_HEAD(&wakeup_irqs);
 	suspend_abort = false;
+}
 
+void clear_wakeup_reasons(void)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&resume_reason_lock, flags);
+	clear_wakeup_reasons_nolock();
 	spin_unlock_irqrestore(&resume_reason_lock, flags);
 }
 
@@ -566,10 +578,12 @@ static int wakeup_reason_pm_event(struct notifier_block *notifier,
 		unsigned long pm_event, void *unused)
 {
 	struct timespec xtom; /* wall_to_monotonic, ignored */
+	unsigned long flags;
 
+	spin_lock_irqsave(&resume_reason_lock, flags);
 	switch (pm_event) {
 	case PM_SUSPEND_PREPARE:
-		clear_wakeup_reasons();
+		clear_wakeup_reasons_nolock();
 
 		get_xtime_and_monotonic_and_sleep_offset(&last_xtime, &xtom,
 			&last_stime);
@@ -603,6 +617,7 @@ static int wakeup_reason_pm_event(struct notifier_block *notifier,
 	default:
 		break;
 	}
+	spin_unlock_irqrestore(&resume_reason_lock, flags);
 	return NOTIFY_DONE;
 }
 
