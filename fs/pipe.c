631e63908aeef2ebae7e35d065a255c8f2e25e06
kldoc <fred.berbigier@gmail.com>
2016-03-27 10:29:45 +0800

    pipe: iovec: derp, missed a block
    missed a block
    
    Change-Id: If76366f632410c4c554ef2b0ec3693747f113f64

--- fs/pipe.c
+++ fs/pipe.c
@@ -115,10 +115,12 @@ pipe_iov_copy_from_user(void *addr, int *offset, struct iovec *iov,
 		copy = min_t(unsigned long, *remaining, iov->iov_len);
 
 		if (atomic) {
-			if (__copy_from_user_inatomic(to, iov->iov_base, copy))
+			if (__copy_from_user_inatomic(addr + *offset,
+						      iov->iov_base, copy))
 				return -EFAULT;
 		} else {
-			if (copy_from_user(to, iov->iov_base, copy))
+			if (copy_from_user(addr + *offset,
+					   iov->iov_base, copy))
 				return -EFAULT;
 		}
 		*offset += copy;
